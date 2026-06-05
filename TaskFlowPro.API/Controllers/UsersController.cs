355313556e5912c6ab0f1eeb993da56b0f583c90
VARSHINI APPANA <170030073@kluniversity.in>
2026-06-05 12:59:20 +0530

    Added Users API

---
+++ TaskFlowPro.API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskFlowPro.API.Models;
+
+namespace TaskFlowPro.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<User>> GetUsers()
+    {
+        var users = new List<User>
+        {
+            new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com"
+            }
+        };
+
+        return Ok(users);
+    }
+}
