fbcda837a5f5d379adf6b50bebd2f61ee2454eb8
VARSHINI APPANA <170030073@kluniversity.in>
2026-06-08 11:47:04 +0530

    Added ApplicationDbContext

---
+++ TaskFlowPro.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using TaskFlowPro.API.Models;
+
+namespace TaskFlowPro.API.Data
+{
+    public class ApplicationDbContext
+    {
+        public List<User> Users { get; set; } = new();
+
+    }
+}
--- TaskFlowPro.API/Services/UserService.cs
+++ TaskFlowPro.API/Services/UserService.cs
@@ -1,21 +1,26 @@
 ﻿using TaskFlowPro.API.Models;
+using TaskFlowPro.API.Data;
 
 namespace TaskFlowPro.API.Services;
 
 public class UserService
 {
+    private readonly ApplicationDbContext _dbContext = new();
     public List<User> GetUsers()
     {
-        return new List<User>
+        if (!_dbContext.Users.Any())
         {
-            new User
-            {
-                Id = 1,
-                FirstName = "John",
-                LastName = "Doe",
-                Email = "john.doe@example.com"
-            }
-        };
+            _dbContext.Users.Add(
+                new User
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "john.doe@example.com"
+                });
+        }
+
+        return _dbContext.Users;
     }
 
     public User GetUserById(int id)
